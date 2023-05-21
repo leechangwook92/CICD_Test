d15f4419621d0669033cd0fff97d1c8171464de4
changwook lee <lcy12225@naver.com>
2023-05-21 21:22:48 +0900

    appspec.yml 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,24 +1,31 @@
-#!/bin/bash
-BUILD_JAR=$(ls /home/ubuntu/action/build/libs/*.jar)
-JAR_NAME=$(basename $BUILD_JAR)
-echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/action/deploy.log
+#!/usr/bin/env bash
 
-echo "> build 파일 복사" >> /home/ubuntu/action/deploy.log
-DEPLOY_PATH=/home/ubuntu/action/
-cp $BUILD_JAR $DEPLOY_PATH
+JAR_REPOSITORY=/home/ubuntu/app/build/libs
+REPOSITORY=/home/ubuntu/app/
+JAR_NAME=$(ls -tr $JAR_REPOSITORY/Backend-0.0.1-SNAPSHOT.jar | tail -n 1)
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/deploy.log
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
+
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
-if [ -z $CURRENT_PID ]
-then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/action/deploy.log
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
+echo "> 새 애플리케이션 배포"
+
+echo "> JAR NAME: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+# -Duser.timezone=Asia/Seoul
+nohup java -jar $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &

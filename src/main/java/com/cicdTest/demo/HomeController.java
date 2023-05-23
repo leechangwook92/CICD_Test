b120c6690bd329f2c66ea91cfb429b1f80c60042
chrislee92 <chrislee.4b@gmail.com>
2023-05-23 17:46:37 +0900

    연결상태 테스트

--- src/main/java/com/cicdTest/demo/HomeController.java
+++ src/main/java/com/cicdTest/demo/HomeController.java
@@ -2,14 +2,16 @@ package com.cicdTest.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
 
     @GetMapping("/hi")
+    @ResponseBody
     public String home() {
-        return "index.html";
+        return "hi";
     }
 
     @GetMapping("/hi2")
